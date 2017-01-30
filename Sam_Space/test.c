b9909ea857e70728fe4f5f292ad000d5659ed59d
sam-rez <sam_rezaie87@yahoo.com>
2017-01-30 11:16:44 -0800

    Did all of task 1 except read/write to disk

--- Sam_Space/test.c
+++ Sam_Space/test.c
@@ -20,8 +20,8 @@ static void generate_random_number(mpz_t *rand_num,
 
 int main() {
    unsigned long int seed, seed2;
-   mpz_t rand_num;
-   gmp_randstate_t rand_state;
+   mpz_t rand_num, rand_num2;
+   gmp_randstate_t rand_state, rand_state2;
   //  mpz_t rand_num2;
   //  gmp_randstate_t rand_state2;
    seed = 123456;
@@ -29,17 +29,63 @@ int main() {
 
    generate_random_number(&rand_num, seed, &rand_state);
    printf("\n");
-   generate_random_number(&rand_num, seed2, &rand_state);
+   generate_random_number(&rand_num2, seed2, &rand_state2);
    printf("\n");
-   //gmp_printf("%Zd\n", rand_num);
-   //check if probabilistically prime
-   int val = mpz_probab_prime_p(rand_num, 30);
-   int val2 = mpz_probab_prime_p(rand_num, 30);
-   printf("%d\n", val);
-   printf("%d\n", val2);
 
+   /*
+   * 2. Checking to see if that integer is (probabilistically) prime
+   * Returns 2 if def prime, 1 if prob prime, 0 if def not prime
+   */
+
+   int val = mpz_probab_prime_p(rand_num, 30);
+   printf("Probabilistically prime?: %d\n", val);
+
+   /*
+   * 3. Printing large integers to stdout
+   * What filestream to print to and what base
+   */
+   mpz_out_str(stdout, 2, rand_num);
+   printf("\n\n");
+   /*
+   * 4. Reading & writing larger integers from/to disk
+   */
+
+
+   /*
+   * 5. Adding and subtracting two large integers
+   */
+   mpz_t sum, difference;
+   mpz_init(sum);
+   mpz_init(difference);
+   mpz_add(sum, rand_num, rand_num2);
+   mpz_sub(difference, rand_num, rand_num2);
+
+   gmp_printf("Sum: %Zd\n\n", sum);
+   gmp_printf("Diff: %Zd\n\n", difference);
+
+   /*
+   * 6. Multiplying two large integers
+   */
+   mpz_t product;
+   mpz_init(product);
+   mpz_mul(product, rand_num, rand_num2);
+   gmp_printf("Product: %Zd\n\n", product);
+
+   /*
+   * 7. Performs modular exponentiation on large integesrs (i.e. a^b mod c)
+   */
+   mpz_t result, exp, mod;
+   mpz_init(result);
+   mpz_init(exp);
+   mpz_set_si(exp, 2);
+   mpz_init(mod);
+   mpz_set_si(mod, 10000000000000);
+   //void mpz_powm(rop, base, exp, mod);
+   mpz_powm(result, rand_num, exp, mod);
+   gmp_printf("Result: %Zd\n\n", result);
    return 0;
 }
+
 
 static void generate_random_number(mpz_t *rand_num,
                                                unsigned long int seed,
